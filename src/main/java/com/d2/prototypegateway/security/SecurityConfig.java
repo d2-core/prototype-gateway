5467a9dda0c13a6945e2f05c4ba8cab9b4fb106a
d2-core <dohaktae88@gmail.com>
2024-12-03 17:24:24 +0900

    application yml url 속성 수정

--- src/main/java/com/d2/prototypegateway/security/SecurityConfig.java
+++ src/main/java/com/d2/prototypegateway/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.d2.prototypegateway.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -9,6 +10,8 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+	@Value("${url.bff-app}")
+	private String bffAppUrl;
 
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
@@ -23,7 +26,7 @@ public class SecurityConfig {
 	@Bean
 	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("http://localhost:3000");
+		config.addAllowedOrigin(bffAppUrl);
 		config.addAllowedMethod("*");
 		config.addAllowedHeader("*");
 		config.setAllowCredentials(true);
